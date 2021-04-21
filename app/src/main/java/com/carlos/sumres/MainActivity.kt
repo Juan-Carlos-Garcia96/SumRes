3e262c9eef13733bb5351166cd5e87bec40ad54e
Carlos Garcia <carlos_garcia_sistemas@outlook.com>
2021-04-20 22:27:31 -0500

    Se agrega evento al oprimir boton de EMPEZAR

--- app/src/main/java/com/carlos/sumres/MainActivity.kt
+++ app/src/main/java/com/carlos/sumres/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.carlos.sumres
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
+
+    fun nextActivity(view: View) {
+        //pendiente, creacion de segunda pantalla
+    }
+
 }
